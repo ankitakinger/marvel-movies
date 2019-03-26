f4f5a5fb6fff81275adb1df94ef138812637d59d
Ankita Kinger <ankitakinger94@gmail.com>
2019-03-26 11:43:33 +0530

    Removed code for Redux Devtools

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './store/reducers/reducer';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? //redux only can be seen in dev n not prod if u use this.. creae react app feature
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? //redux only can be seen in dev n not prod if u use this.. creae react app feature
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, (applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
